ea1ebee06663f9040fd8f3600227aad44454750a
daniel campo <campo_daniel94@yahoo.com>
2020-08-11 14:59:27 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,20 @@
-# Your code goes here!
+class Dog
+ def name=(dog)
+   @fido = dog
+  end
+  
+    def name
+    @fido
+end
+
+  def bark
+    puts "woof!"
+  
+end
+end
+
+fido = Dog.new
+fido.name = "fido"
+
+
+
